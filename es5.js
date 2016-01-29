50eef3f00ab7310764e8bf8ac477efcc3d6a727a
Joel Santos <joel@sendoushi.com>
2016-01-29 15:56:11 +0000

    [new] ES5

---
+++ es5.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+    "rules": {
+        "strict": [2, "global"],
+        "indent": [2, 4],
+        "comma-dangle": [2, "never"]
+    },
+    "extends": [
+        "defaults/configurations/airbnb/es5"
+    ]
+};
