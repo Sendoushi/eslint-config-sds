9a0c15ac455d42b88f80789fa8f04dc3fe620c96
Joel Santos <joel@sendoushi.com>
2016-02-24 18:27:12 +0000

    [upd] Remove no param reassign

--- eslint.js
+++ eslint.js
@@ -6,8 +6,7 @@ module.exports = {
         "indent": [2, 4],
         "comma-dangle": [2, "never"],
         "func-names": [0],
-        "no-param-reassign": [1],
-        "consistent-return": [0],
-        "prefer-const": [0]
+        "no-param-reassign": [0],
+        "consistent-return": [0]
     }
 };
