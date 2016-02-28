c664ca6592fd6124d64f61fb1aa1e47fe135194e
Joel Santos <joel@sendoushi.com>
2016-02-28 16:37:04 +0000

    [upd] React rule

--- es6-react.js
+++ es6-react.js
@@ -4,5 +4,8 @@ module.exports = {
     "extends": [
         "defaults/configurations/airbnb/es6-react",
         "sds"
-    ]
+    ],
+    "rules": {
+        "react/prefer-es6-class": [0]
+    }
 };
